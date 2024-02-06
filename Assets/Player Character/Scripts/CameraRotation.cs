bb4aabdc63db2150b337aaf0e4f08214c341c923
vadimsman <vadimsman@mail.ru>
2024-02-06 20:37:52 +0300

    1.0

---
+++ Assets/Player Character/Scripts/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float RotationSpeed;
+
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+    }
+}
