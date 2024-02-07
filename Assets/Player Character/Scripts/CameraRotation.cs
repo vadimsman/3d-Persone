9274785fddcf1106d30c58069fc02098a692844f
vadimsman <vadimsman@mail.ru>
2024-02-07 20:24:23 +0300

    1.1

--- Assets/Player Character/Scripts/CameraRotation.cs
+++ Assets/Player Character/Scripts/CameraRotation.cs
@@ -10,10 +10,23 @@ public class CameraRotation : MonoBehaviour
         
     }
 
+    public Transform CameraAxisTransform;
     public float RotationSpeed;
+    public float MaxAngle;
+    public float MinAngle;
+
 
     void Update()
     {
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+        var NewAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+
+        if (NewAngleX > 180)
+            NewAngleX -= 360;
+
+        NewAngleX = Mathf.Clamp(NewAngleX, MinAngle, MaxAngle);
+
+        CameraAxisTransform.localEulerAngles = new Vector3(NewAngleX, 0, 0);
     }
 }
